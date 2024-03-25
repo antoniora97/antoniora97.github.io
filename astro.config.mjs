66f70dda54448ccecb87c1778b6ddc0fb78720d0
antoniora97 <antoniora1997@gmail.com>
2024-03-25 20:49:36 +0100

    fix: CNAME file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://antoniora97.github.io",
+  site: "https://antoniora97",
   base: "aprendiendo-rxjs",
 });
