b0f830a3b37ab6c25c77ec23336adc062e45e2f4
antoniora97 <antoniora1997@gmail.com>
2024-03-25 20:38:44 +0100

    fix: remove domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://aprendiendo-rxjs.com',
-    base: 'aprendiendo-rxjs'
+  site: "https://antoniora97.github.io",
+  base: "aprendiendo-rxjs",
 });
