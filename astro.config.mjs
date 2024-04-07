04c8fbc5744e5ec3034f4001d5b1b62714855f4b
antoniora97 <antoniora1997@gmail.com>
2024-04-07 16:16:06 +0200

    test: restore astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,4 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://antoniora97.github.io",
-//   vite: {
-//     plugins: [
-//       {
-//         name: "prismjs",
-//         setup() {
-//           return {
-//             onMount() {
-//               prism.highlightAll();
-//             },
-//           };
-//         },
-//       },
-//     ],
-//   },
 });
