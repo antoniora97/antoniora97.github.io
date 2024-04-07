94c2ef3234ab6bcd65120755290f59376656fa5f
antoniora97 <antoniora1997@gmail.com>
2024-04-07 16:13:29 +0200

    add: exercises page and prism.js for code style

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,19 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://antoniora97.github.io',
+  site: "https://antoniora97.github.io",
+//   vite: {
+//     plugins: [
+//       {
+//         name: "prismjs",
+//         setup() {
+//           return {
+//             onMount() {
+//               prism.highlightAll();
+//             },
+//           };
+//         },
+//       },
+//     ],
+//   },
 });
